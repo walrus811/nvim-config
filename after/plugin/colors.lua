0c6cf03f61aeebb747a376417223c6db0a66cd34
rayray <walrusray811@gmail.com>
2023-04-04 11:33:00 +0900

    add plugins!

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -1,5 +1,14 @@
 function SetColor(color)
 	color = color or "tokyonight"
+
+	if color == "tokyonight" then
+		require("tokyonight").setup({
+			style = "storm", -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
+			light_style = "day", -- The theme is used when the background is set to light
+			transparent = true, -- Enable this to disable setting the background color
+		})
+	end
+
 	vim.cmd.colorscheme(color)
 
 	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
--- lua/ray/packer.lua
+++ lua/ray/packer.lua
@@ -17,5 +17,7 @@ return require('packer').startup(function(use)
 
   use ('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
   use ('nvim-treesitter/playground')
-
+  use ('ThePrimeagen/harpoon')
+  use ('mbbill/undotree')
+  use ('tpope/vim-fugitive')
 end)
