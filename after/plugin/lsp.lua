836d909e08daa59c4e8b20fd9d798099b9f0ffcf
rayray <walrusray811@gmail.com>
2023-04-05 18:58:41 +0900

    add format on save for eslint

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -30,6 +30,10 @@ lsp.setup_nvim_cmp({
 lsp.on_attach(function(client, bufnr)
   local opts = {buffer = bufnr, remap = false}
 
+  if (client.name == "eslint") then
+      vim.cmd('autocmd BufWritePre *.tsx,*.ts,*.jsx,*.js EslintFixAll')
+  end
+
   vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
   vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
   vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
